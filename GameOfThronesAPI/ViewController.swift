d8f5259f2c634a4994497b2d915dee44a617b2d3
Egor Ukolov <ukolovegor@yandex.ru>
2024-07-04 19:17:18 +0700

    Initial Commit

---
+++ GameOfThronesAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameOfThronesAPI
+//
+//  Created by Egor Ukolov on 04.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
