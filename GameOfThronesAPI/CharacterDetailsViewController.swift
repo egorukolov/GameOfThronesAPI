4694c4913f79acc74b9a3c711d15d6103e8fd0ac
Egor Ukolov <ukolovegor@yandex.ru>
2024-07-05 18:34:41 +0700

    Реализация экрана CharacterDetailViewController. Перпедача данных по выбранной ячейке таблицы

---
+++ GameOfThronesAPI/CharacterDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CharacterDetailsViewController.swift
+//  GameOfThronesAPI
+//
+//  Created by Egor Ukolov on 05.07.2024.
+//
+
+import UIKit
+
+class CharacterDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
