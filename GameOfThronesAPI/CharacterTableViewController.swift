bc5f454ce0f7ede121abc0dcd75fda888a22a8e0
Egor Ukolov <ukolovegor@yandex.ru>
2024-07-05 14:02:11 +0700

    Реализации таблицы с именами персонажей; сетевой запрос с порлучением данных от API

--- GameOfThronesAPI/CharacterTableViewController.swift
+++ GameOfThronesAPI/CharacterTableViewController.swift
@@ -7,39 +7,31 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class CharacterTableViewController: UITableViewController {
+    
+    let characters: [Character] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        title = "Thrones Characters"
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        characters.count
+        
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let character = characters[indexPath.row]
+        cell.textLabel?.text = character.fullName
         return cell
     }
-    */
+   
 
     /*
     // Override to support conditional editing of the table view.
